f0225feed5759028b812241189e0d755472f28da
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-04-27 19:10:42 +0900

    almost finished
    trying to change the button and font

--- todo.js
+++ todo.js
@@ -4,15 +4,45 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+let toDos = []; //빈 어레이를 만들어서 투두리스트를 다 넣어줄 거야
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+    console.log(toDo.id, li.id);
+  });
+  toDos = cleanToDos;
+  saveTodos();
+}
+
+function saveTodos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //localStorage는 자바스크립트를 스트링으로 저장하기때문에
+  //object를 스트링으로 변환시켜줘야해
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj); //toDoObj 내용을 toDo[어레이]안에 넣어줄 거야
+  saveTodos(); //obj 넣어준 후에 불러야 안에 내용물이 담긴다능
 }
 
 function handleSubmit(event) {
@@ -23,8 +53,14 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos); //텍스트를 다시 js 오브젝트로 전환
+    parsedToDos.forEach(
+      function (toDo) {
+        paintToDo(toDo.text);
+      } //어레이에 담겨있는 것들 각각에 한번씩 함수를 실행시켜준다
+    );
   }
 }
 
---
+++ weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "93414c84efd5f471c5ba845163afab12";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      const temperature = Math.round(json.main.temp);
+      const place = json.name;
+      const country = json.sys.country;
+      weather.innerText = `${temperature}℃ @ ${place} - ${country}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude: latitude,
+    longitude: longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+function handleGeoError() {
+  console.log("cant access geo location");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    //getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+init();
